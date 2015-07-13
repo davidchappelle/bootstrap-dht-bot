6b7863ef395c9531882685c33c453246ee70d6cc
David Chappelle <dchappelle@incognito.com>
2015-07-13 12:58:23 -0400

    Update bot to take in a session port
    This allows us to run multiple bot instances on the same host machine.

--- main.cpp
+++ main.cpp
@@ -11,7 +11,7 @@ using namespace libtorrent;
 
 void usage()
 {
-	std::cerr << "USAGE: bootstrap-dht-bot <host> <port>" << std::endl;
+	std::cerr << "USAGE: bootstrap-dht-bot <server-host> <server-port> <session-port>" << std::endl;
 	exit(1);
 }
 
@@ -28,34 +28,44 @@ void dispatch_libtorrent_alert(std::shared_ptr<session> s, std::auto_ptr<libtorr
 
 int main(int argc, char* argv[])
 {
-	if (argc != 3) {
+    if (argc != 4) {
         usage();
     }
 
-    const char* host = argv[1];
-    int port = atoi(argv[2]);
+    const char* server_host = argv[1];
+    int server_port = atoi(argv[2]);
+    int session_port = atoi(argv[3]);
     int flags = 0;
 
-    if (port == 0) {
-        std::cerr << "error: port is invalid" << std::endl;
+    if (server_port == 0) {
+        std::cerr << "error: server port '" << server_port << "' is invalid" << std::endl;
+        exit(-1);
+    }
+
+    if (session_port == 0) {
+        std::cerr << "error: session port '" << session_port << "' is invalid" << std::endl;
         exit(-1);
     }
 
     libtorrent::fingerprint fp("LT", LIBTORRENT_VERSION_MAJOR, LIBTORRENT_VERSION_MINOR, 0, 0);
-    std::shared_ptr<libtorrent::session> s = std::make_shared<libtorrent::session>(
-            fp, std::make_pair(port, port + 8), "0.0.0.0", flags,
+    std::shared_ptr<libtorrent::session> session = std::make_shared<libtorrent::session>(
+            fp, std::make_pair(session_port, session_port + 8), "0.0.0.0", flags,
             libtorrent::alert::port_mapping_notification |
             libtorrent::alert::debug_notification |
             libtorrent::alert::dht_notification |
             libtorrent::alert::error_notification |
             libtorrent::alert::status_notification);
 
-    s->set_alert_dispatch(std::bind(&dispatch_libtorrent_alert, s, std::placeholders::_1));
-    s->add_dht_router(std::make_pair(host, 6881));
-    s->start_dht();
+    session->set_alert_dispatch(std::bind(&dispatch_libtorrent_alert, session, std::placeholders::_1));
+    session->add_dht_router(std::make_pair(server_host, server_port));
+    session->start_dht();
 
     // Just block here and let the session run forever.
     std::mutex forever;
     forever.lock();
     forever.lock();
+
+    std::cerr << "stopping bot" << std::endl;
+
+    return 0;
 }
